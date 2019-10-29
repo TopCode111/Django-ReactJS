2207787f015979ef4571ac82b9208e35f1228ac4
AldahirMartinez <aldahir.martinez9197@gmail.com>
2019-10-28 20:15:47 -0400

    removed logout to log out

--- frontend/src/components/portal/Dashboard.js
+++ frontend/src/components/portal/Dashboard.js
@@ -9,15 +9,11 @@ class Dashboard extends Component {
     render() {
         return (
             <div className="dashboard-container">
-                <center>
                 <img className='dashboard-icon' src="static/frontend/images/icon.png" />
-                </center>
-                <div className="pure-g">
-                    <div className="pure-u-1-6">
-                        <div className="logout"><Logout /></div>
-                    </div>
-                </div>
-                <div className="center dashboard-heading">Welcome, Dr {this.props.auth.user.first_name + ' ' + this.props.auth.user.last_name}</div>
+                     <div className="logout right"><Logout /></div>
+			    <div>
+					 <div className="center dashboard-heading">Welcome, Dr {this.props.auth.user.first_name + ' ' + this.props.auth.user.last_name}</div>
+			    </div>
                 <div className="pure-g">
                     <div className="pure-u-1-6">
                         <div className="sidebar">
@@ -25,7 +21,7 @@ class Dashboard extends Component {
                         </div>
                     </div>
                     <div className="pure-u-5-6">
-                        <PatientList tableTitle="Patients"/>
+                        <PatientList tableTitle="Patients" doctor={this.props.auth.user} />
                     </div>
                 </div>
             </div>
