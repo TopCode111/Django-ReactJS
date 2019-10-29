2d5262f066de02f96a98763546fd345ac801cc55
AldahirMartinez <aldahir.martinez9197@gmail.com>
2019-10-28 20:10:00 -0400

    added icon to patient

--- frontend/src/components/common/Logout.js
+++ frontend/src/components/common/Logout.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
+
 export class Logout extends Component {
   static propTypes = {
     auth: PropTypes.object.isRequired,
@@ -10,7 +11,7 @@ export class Logout extends Component {
   render() {
       return (
           <span onClick={this.props.logout} >
-            Logout
+            Log out
           </span>
     );
   }
