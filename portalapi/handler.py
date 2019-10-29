9dd6f9deb65893218b50b221db42c1519e1a44de
AldahirMartinez <aldahir.martinez9197@gmail.com>
2019-10-28 20:01:50 -0400

    update

--- portalapi/handler.py
+++ portalapi/handler.py
@@ -7,7 +7,7 @@ def send_email(name, email, date, path):
         'Telemonica 360',
         """
         Dear {0}, you agreed on {1} to have regular follow-up by your doctor. Please click on this link to
-        start the questionnaire: {2}.If you encounter any technical problem please contact Telemonica
+        start the questionnaire: {2}. If you encounter any technical problem please contact Telemonica
         Support Centre.""".format(name, date, path),
         settings.EMAIL_HOST_USER,
         [email]
