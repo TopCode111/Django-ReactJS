44e754f0cf0e9d5960e9d2442cd7298a60b7a785
AldahirMartinez <aldahir.martinez9197@gmail.com>
2019-10-28 20:08:16 -0400

    removed contact

--- frontend/src/components/auth/Header.js
+++ frontend/src/components/auth/Header.js
@@ -1,37 +1,23 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     return (
-      <div className="header-parent">
-        <div className="header-language">
-          <span className="language">
-            <Link to="/">HOME</Link>
-          </span>
-
-          {/* <span className="language">CONTACT</span>
-
-			<span>FR</span>/<span className="selected">EN</span> */}
-        </div>
-        {this.props.shouldShowBack && (
-          <div
-            className="back-link pointer"
-            onClick={() => this.props.backToStep()}
-          >
-            {" "}
-            Back
-          </div>
-        )}
-        {this.props.shouldShowHome && (
-          <div
-            className="header-element"
-            onClick={() => (window.location.href = "/")}
-          >
-            Home
-          </div>
-        )}
-      </div>
-    );
+		<div className='header-parent'>
+		    { this.props.showRight &&
+			      <div className="header-language">
+				      <span className="language"><Link to="/">HOME</Link></span>        
+      			 	  <span className="language">CONTACT</span>
+			      </div>
+		    }
+    		{
+				this.props.shouldShowBack && <div className="back-link pointer" onClick={() => this.props.backToStep()} >  Back</div>
+   	 		}
+    		{
+				this.props.shouldShowHome && <div className="header-element" onClick={() => window.location.href="/" } >Home</div>
+   	 		}
+		</div>
+    )
   }
 }
